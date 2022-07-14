db0baae6e98362108c53240be312b85a8072423a
Samir Anan <samir.anan.dev@gmail.com>
2022-07-14 12:28:49 +0300

    models functionalities

---
+++ app/Models/City.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class City extends Model
+{
+    use HasFactory;
+}
+
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class City extends Model
+{
+    use HasFactory ,SoftDeletes;
+
+    protected $fillable = ['name' , 'slug' , 'country_id'];
+
+    protected $dates = [
+        'deleted_at'
+    ];
+
+    public function setSlugAttribute($value)
+    {
+        $this->attributes['slug'] = strtoupper($value);
+    }
+
+
+    public function country(){
+        return $this->belongsTo(Country::class);
+    }
+
+
+    public function users(){
+        return $this->belongsTo(User::class);
+    }
+
+}
